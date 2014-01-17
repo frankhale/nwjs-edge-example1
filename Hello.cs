82d35bd38735f71605731c7706b806a90821c2a0
Frank Hale <frankhale@gmail.com>
2014-01-16 20:59:17 -0500

    Add Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLibrary
+{
+    public class HelloWorld
+    {
+			public async Task<object> SayHello(object input)
+			{
+				return await Task.FromResult<string>(string.Format("Hello {0} From C#", input));
+			}
+    }
+}
